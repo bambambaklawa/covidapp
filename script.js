7629983598e3fc4887bbb3138664f38f2f53e6f9
bambambaklawa <58434653+bambambaklawa@users.noreply.github.com>
2020-05-10 10:09:09 +0200

    updatemay10

--- script.js
+++ script.js
@@ -1,128 +1,96 @@
-const URL = "https://corona.lmao.ninja/v2/";
-let map;
-let infoWindow;
-let markers = [];
-let mykey = "AIzaSyDmZQImxjUjMf3-nX8m4h30SMNp-LqS1p4";
-
-let script = document.createElement("script");
-
-script.src = `https://maps.googleapis.com/maps/api/js?key=${mykey}&callback=initMap`;
-script.defer = true;
-script.async = true;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 0,
-      lng: 0,
-    },
-    zoom: 2,
-  });
-  infoWindow = new google.maps.InfoWindow();
-}
-
-document.body.appendChild(script);
-
-window.onload = function () {
+window.onload = () => {
   getGlobalData();
   getCountryMarkers();
 };
 
-getCountryMarkers = () => {
-  const FULL_URL = "https://corona.lmao.ninja/v2/countries?yesterday=&sort=";
-
-  const globalPromise = fetch(FULL_URL);
-
-  globalPromise
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      showCountryMarkers(data);
+var map;
+var infoWindow;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
     });
-};
-
-const onEnter = (e) => {
-  if (e.key === "Enter") {
-    getCountryData();
   }
-};
 
-getCountryData = (country = "") => {
-  if (country == "") {
-    country = document.getElementById("search-country").value;
-  }
-  const FULL_URL = `${URL}countries/${country}?yesterday=true&strict=true&query`;
-
-  const globalPromise = fetch(FULL_URL);
-
-  globalPromise
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      showCountryData(data);
-    });
-};
-
-showCountryData = (countryData) => {
-  document.getElementById(
-    "country"
-  ).innerText = `${countryData.country.toUpperCase()}`;
-  document.getElementById(
-    "flag"
-  ).innerHTML = `<img src='${countryData.countryInfo.flag}' alt='Country flag'>`;
-  document.getElementById(
-    "today-country-cases"
-  ).innerText = `NEW CASES: ${countryData.todayCases}`;
-  document.getElementById(
-    "today-country-deaths"
-  ).innerText = `NEW DEATHS: ${countryData.todayDeaths}`;
-  document.getElementById(
-    "active-cases"
-  ).innerText = `ACTIVE CASES: ${countryData.active}`;
-};
-
-function showCountryMarkers(countries) {
-  let totalCases = 0;
-  let recovered = 0;
-  let totalDeaths = 0;
-  let name;
-  var bounds = new google.maps.LatLngBounds();
-  countries.forEach(function (country, index) {
-    var latlng = new google.maps.LatLng(
-      country.countryInfo.lat,
-      country.countryInfo.long
-    );
-    name = country.country;
-    totalCases = country.cases;
-    totalDeaths = country.deaths;
-    recovered = country.recovered;
-    createMarker(latlng, name, totalCases, totalDeaths, recovered, index);
-    bounds.extend(latlng);
-  });
-  map.panToBounds(bounds);
+const GetCountryData = () => {
+  fetch("https://corona.lmao.ninja/v2/countries")
+  .then((response) => {
+    return response.json()
+  }).then((data) => {
+    showDataOnMap(data);
+    showDataInTable(data);
+  })
 }
 
-function createMarker(latlng, name, totalCases, totalDeaths, recovered, index) {
-  let country = name;
-  var html = `<h3>${country}</h3>
-                <p>Total cases: ${totalCases}</p>
-                <p>Total Deaths: ${totalDeaths}</p>
-                <p>Recovered: ${recovered}</p>
-                `;
-  var marker = new google.maps.Marker({
+const showDataOnMap = (data) => {
+
+data.map((country)=>{
+
+  let countryCenter = {
+    lat: country.countryInfo.lat,
+    lng: country.countryInfo.lng
+  }
+
+  var cityCircle = new google.maps.Circle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
     map: map,
-    position: latlng,
-    label: "\u29BF",
+    center: countryCenter,
+    radius: country.casesPerOneMillion * 10
   });
 
-  google.maps.event.addListener(marker, "click", function () {
-    infoWindow.setContent(html);
-    infoWindow.open(map, marker);
-    getCountryData(country);
+  var html = `
+  <div class="info-container">
+    <div class="info-flag" style="background-image: url(${countryInfo.flag}">
+    <img src="${country.countryInfo.flag}"/>
+    </div>
+    <div class="info-name">
+    ${country.country}
+    </div>
+    <div class="info-confirmed>
+    Total: ${country.cases}
+    </div>
+    <div class="info-recovered">
+    Recovered: ${country.recovered}
+    </div>
+    <div class="info-deaths">
+    Deaths: ${country.deaths}
+    </div>
+  </div>
+  `
+
+var infoWindow = new google.maps.InfoWindow({
+  content: html,
+  position: countryCircle.center
+})
+
+  google.maps.event.addListener(countryCircle, "mouseover", function() {
+    infoWindow.open(map);
   });
 
-  markers.push(marker);
+  google.maps.event.addListener(countryCircle, "mouseout", function(){
+    infoWindow.close();
+  })
+
+}
+
+}
+
+const showDataInTable = (data) => {
+  var html = "";
+  data.forEach((country)=>{
+
+    html += `
+    <tr>
+    <td>${country.country}</td>
+    <td>${country.cases}</td>
+    <td>${country.recovered}</td>
+    <td>${country.deaths}</td>
+  </tr>
+      `
+  })
+  document.getElementById("table-data").innerHTML = html;
 }
